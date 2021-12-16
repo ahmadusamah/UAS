d70c1b6f692903997171d19caa91d2ac4d4cb7f9
ahmadusamah <95966704+ahmadusamah@users.noreply.github.com>
2021-12-16 22:28:09 +0700

    Update UAS.py

--- UAS.py
+++ UAS.py
@@ -89,45 +89,41 @@ pilih_tahun_3 = right_col.slider("Pilih tahun:", 1971, 2015, 2001)
 right_col.subheader("Data produsen minyak")
 data4 = oil_data.loc[oil_data["tahun"] == pilih_tahun_3]
 data4 = data4.sort_values(["produksi"], ascending=[0])
-
-st.write(data4.groupby("kode_negara"))
+st.write(data4.groupby("kode_negara")["produksi"].max())
 
 st.subheader("Data Kumulatif")
 data5 = oil_data.groupby("kode_negara")["produksi"].sum()
 
-def test(data):
-	max_value, max_index, min_value, min_index = (None, )*4
-	nama = list()
-	kodenegara = list()
-	region = list()
-	subregion = list()
-	for index, value in data.items():
-		if max_value is None or value > max_value :
-			max_value = value
-			max_index = index
-		if min_value is None or value < min_value and value != 0 :
-			min_value = value
-			min_index = index
-		if value == 0:
-			for y in kode:
-				kode2 = y['alpha-3']
-				if index == kode2 :
-					nama.append(y['name'])
-					kodenegara.append(y["country-code"])
-					region.append(y["region"])
-					subregion.append(y["sub-region"])
+max_value, max_index, min_value, min_index = (None, )*4
+nama = list()
+kodenegara = list()
+region = list()
+subregion = list()
+for index, value in data.items():
+	if max_value is None or value > max_value :
+		max_value = value
+		max_index = index
+	if min_value is None or value < min_value and value != 0 :
+		min_value = value
+		min_index = index
+	if value == 0:
+		for y in kode:
+			kode2 = y['alpha-3']
+			if index == kode2 :
+				nama.append(y['name'])
+				kodenegara.append(y["country-code"])
+				region.append(y["region"])
+				subregion.append(y["sub-region"])
 
-	for y in kode:
-		kode2 = y['alpha-3']
-		if max_index == kode2 :
-			st.write('Nama Negara :',y['name'],'\nKode Negara :',y["country-code"],'\nRegion :',y["region"],'\nSub-region :',y["sub-region"],'\nTotal Produksi :',max_value)
-		if min_index == kode2 :
-			st.write('Nama Negara :',y['name'],'\nKode Negara :',y["country-code"],'\nRegion :',y["region"],'\nSub-region :',y["sub-region"],'\nTotal Produksi :',min_value)
+for y in kode:
+	kode2 = y['alpha-3']
+	if max_index == kode2 :
+		st.write('Nama Negara :',y['name'],'\nKode Negara :',y["country-code"],'\nRegion :',y["region"],'\nSub-region :',y["sub-region"],'\nTotal Produksi :',max_value)
+	if min_index == kode2 :
+		st.write('Nama Negara :',y['name'],'\nKode Negara :',y["country-code"],'\nRegion :',y["region"],'\nSub-region :',y["sub-region"],'\nTotal Produksi :',min_value)
 
-	tabel = {'Nama Negara':nama,'Kode Negara':kodenegara,'Region':region,'Sub-region':subregion}
-	df = pd.DataFrame(data = tabel)
-	st.dataframe(df)
-	
-test(data5)
-test(data4)
+tabel = {'Nama Negara':nama,'Kode Negara':kodenegara,'Region':region,'Sub-region':subregion}
+df = pd.DataFrame(data = tabel)
+st.dataframe(df)
+
 
