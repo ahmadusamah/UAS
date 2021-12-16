2b75d30f5adbae9ea0b60ae2364044d6369275ba
ahmadusamah <95966704+ahmadusamah@users.noreply.github.com>
2021-12-16 13:20:22 +0700

    Update UAS.py

--- UAS.py
+++ UAS.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import streamlit as st 
 import random
 
-st.title('Aplikasi Data Produksi Minyak Dunia')
+st.title('Data Produksi Minyak Dunia')
 
 oil_data = pd.read_csv(r'produksi_minyak_mentah.csv')
 info_negara = open(r'kode_negara_lengkap.json')
@@ -17,4 +17,23 @@ for negara in kode :
     daftar_negara.append(negara["name"])
 warna = ("r","g","b","c","m","k")
 excluded = ['WLD','G20','OECD','OEU','EU28']
+#============Soal Pertama================
+pilih_negara = st.selectbox('Pilih Negara',daftar_negara)
+pilih_kode_negara = None
+for negara in kode :
+	if pilih_negara == negara ["name"] :
+		pilih_kode_negara = negara ["alpha-3"]
+
+st.write(pilih_negara,pilih_kode_negara)
+
+data = oil_data.loc[oil_data["kode_negara"] == pilih_kode_negara]   
+data["tahun"] = data["tahun"].astype(int) 
+data["produksi"] = data["produksi"].astype(int)     
+
+data.plot(kind="line", x="tahun", y="produksi",c=random.choice(warna))
+plt.title("Produksi Minyak ",pilih_negara)
+plt.xlabel("Tahun Produksi")
+plt.ylabel("Jumlah Produksi")
+plt.show()
+
 
